0280501d8c09cc25d542f7b91dcbb5ef8888baa4
Colton Williams <colton.williams@activelogic.com>
2024-02-22 19:16:17 -0600

    editing todos

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,43 +1,112 @@
+import { useState } from 'react';
 import '../reset.css';
 import '../App.css';
-import { useState } from 'react';
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: 'Finish React Series',
+      isComplete: false,
+      isEditing: false,
+    },
+    {
+      id: 2,
+      title: 'Go Grocery',
+      isComplete: true,
+      isEditing: false,
+    },
+    {
+      id: 3,
+      title: 'Take over world',
+      isComplete: false,
+      isEditing: false,
+    },
+  ]);
 
-    const [todos, setTodos] = useState([
-        { id: 1, title: 'Finish React Series', isComplete: false },
-        { id: 2, title: 'Go to Grocery', isComplete: true },
-        { id: 3, title: 'Do other thing', isComplete: false }
+  const [todoInput, setTodoInput] = useState('');
+  const [idForTodo, setIdForTodo] = useState(4);
+
+  function addTodo(event) {
+    event.preventDefault();
+
+    if (todoInput.trim().length === 0) {
+      return;
+    }
+
+    setTodos([
+      ...todos,
+      {
+        id: idForTodo,
+        title: todoInput,
+        isComplete: false,
+      },
     ]);
 
-    function addTodo(event) {
-        event.preventDefault();
+    setTodoInput('');
+    setIdForTodo(prevIdForTodo => prevIdForTodo + 1);
+  }
 
-        if (todoInput.trim() === '') {
-            return;
+  function deleteTodo(id) {
+    setTodos([...todos].filter(todo => todo.id !== id));
+  }
+
+  function handleInput(event) {
+    setTodoInput(event.target.value);
+  }
+
+  function completeTodo(id) {
+    const updatedTodos = todos.map(todo => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+
+      return todo;
+    });
+
+    setTodos(updatedTodos);
+  }
+
+  function markAsEditing(id) {
+    const updatedTodos = todos.map(todo => {
+      if (todo.id === id) {
+        todo.isEditing = true;
+      }
+
+      return todo;
+    });
+
+    setTodos(updatedTodos);
+  }
+
+  function updateTodo(event, id) {
+    const updatedTodos = todos.map(todo => {
+      if (todo.id === id) {
+        if (event.target.value.trim().length === 0) {
+          todo.isEditing = false;
+          return todo;
         }
+        todo.title = event.target.value;
+        todo.isEditing = false;
+      }
 
-        setTodos([...todos, { 
-            id: idForTodo,
-            title: todoInput,
-            isComplete: false 
-            }
-        ]);
+      return todo;
+    });
 
-        setTodoInput('');
-        setIdForTodo(prevIdForTodo => prevIdForTodo + 1);
-    }
+    setTodos(updatedTodos);
+  }
 
-    function deleteTodo (id) {
-        return () => setTodos(todos.filter(todo => todo.id !== id));
-    }
+  function cancelEdit(event, id) {
+    const updatedTodos = todos.map(todo => {
+      if (todo.id === id) {
+        todo.isEditing = false;
+      }
 
-    const [todoInput, setTodoInput] = useState('Something');
-    const [idForTodo, setIdForTodo] = useState(4);
+      return todo;
+    });
 
-    function handleInput(e) {
-        setTodoInput(e.target.value);
-    }
+    setTodos(updatedTodos);
+  }
 
   return (
     <div className="todo-app-container">
@@ -45,40 +114,67 @@ function App() {
         <h2>Todo App</h2>
         <form action="#" onSubmit={addTodo}>
           <input
+            type="text"
             value={todoInput}
             onChange={handleInput}
-            type="text"
             className="todo-input"
             placeholder="What do you need to do?"
           />
         </form>
 
         <ul className="todo-list">
-            {todos.map((todo, index) => (
-                <li key={todo.id} className="todo-item-container">
-                    <div className="todo-item">
-                    <input type="checkbox" />
-                    <span className="todo-item-label">{todo.title}</span>
-                    {/* <input type="text" className="todo-item-input" value="Finish React Series" /> */}
-                    </div>
-                    <button onClick={deleteTodo(todo.id)} className="x-button">
-                    <svg
-                        className="x-button-icon"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                    >
-                        <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M6 18L18 6M6 6l12 12"
-                        />
-                    </svg>
-                    </button>
-                </li>
-            ))
-            }
+          {todos.map((todo, index) => (
+            <li key={todo.id} className="todo-item-container">
+              <div className="todo-item">
+                <input
+                  type="checkbox"
+                  onChange={() => completeTodo(todo.id)}
+                  checked={todo.isComplete ? true : false}
+                />
+
+                {!todo.isEditing ? (
+                  <span
+                    onDoubleClick={() => markAsEditing(todo.id)}
+                    className={`todo-item-label ${
+                      todo.isComplete ? 'line-through' : ''
+                    }`}
+                  >
+                    {todo.title}
+                  </span>
+                ) : (
+                  <input
+                    type="text"
+                    onBlur={event => updateTodo(event, todo.id)}
+                    onKeyDown={event => {
+                      if (event.key === 'Enter') {
+                        updateTodo(event, todo.id);
+                      } else if (event.key === 'Escape') {
+                        cancelEdit(event, todo.id);
+                      }
+                    }}
+                    className="todo-item-input"
+                    defaultValue={todo.title}
+                    autoFocus
+                  />
+                )}
+              </div>
+              <button onClick={() => deleteTodo(todo.id)} className="x-button">
+                <svg
+                  className="x-button-icon"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
+            </li>
+          ))}
         </ul>
 
         <div className="check-all-container">
